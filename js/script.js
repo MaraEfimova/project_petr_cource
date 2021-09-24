e9a038d34796e1eda08c2160adb722cc3e40783f
Maria Efimova <marivaefi@gmail.com>
2021-09-24 11:07:01 +0300

    function test

--- js/script.js
+++ js/script.js
@@ -1,4 +1,67 @@
-"use strict";
+let numberOfFilms;
 
-let number = 4.6;
+function start() {
+    numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+    };
+};
+
+start();
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: {},
+    privat: false
+};
  
+
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотреных фильмов,', ''),
+              b = prompt('На сколько оцените его?', '');
+        
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
+    }
+}
+
+// rememberMyFilms();
+
+function detectPersonalLevel () {
+    if (personalMovieDB.count < 10) {
+        console.log('malo');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log('SREDNE');
+    } else if (personalMovieDB.count >= 30) {
+        console.log('kinoman');
+    } else {
+        console.log('error');
+    }
+};
+
+// detectPersonalLevel();
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    };
+};
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`, '');
+    }
+};
+
+writeYourGenres();
